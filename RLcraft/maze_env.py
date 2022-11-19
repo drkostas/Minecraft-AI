54d5e7984f36a267fdecfc14297bf0f2cf6e92ca
drkostas <georgiou.kostas94@gmail.com>
2022-11-19 22:32:37 +0000

    Finalized library selection, created simple agent

--- RLcraft/maze_env.py
+++ RLcraft/maze_env.py
@@ -5,192 +5,216 @@ import time
 import numpy as np
 from enum import Enum
 
-CLIENT_PORT = 9000        # malmo port
-TIME_WAIT = 0.05          # time to wait for retreiving world state (when MsPerTick=20)
-MAX_LOOP = 50             # wait till TIME_WAIT * MAX_LOOP seconds for each action
+CLIENT_PORT = 9000                  # malmo port
+TIME_WAIT = 0.05                    # time to wait for retreiving world state (when MsPerTick=20)
+MAX_LOOP = 50                       # wait till TIME_WAIT * MAX_LOOP seconds for each action
 
 class AgentActionSpace(gym.spaces.Discrete):
-  def __init__(self):
-    actions = []
-    actions.append("move")
-    actions.append("right")
-    actions.append("left")
-    self.actions = actions
-    gym.spaces.Discrete.__init__(self, len(self.actions))
+    def __init__(self):
+        actions = []
+        actions.append("move")
+        actions.append("right")
+        actions.append("left")
+        self.actions = actions
+        gym.spaces.Discrete.__init__(self, len(self.actions))
 
-  def sample(self):
-    return random.randint(1, len(self.actions)) - 1
+    def sample(self):
+        return random.randint(1, len(self.actions)) - 1
 
-  def __getitem__(self, action):
-    return self.actions[action]
+    def __getitem__(self, action):
+        return self.actions[action]
 
-  def __len__(self):
-    return len(self.actions)
+    def __len__(self):
+        return len(self.actions)
 
 class MalmoMazeEnv(gym.Env):
-  """
-  A class implementing OpenAI gym environment to
-  run Project Malmo 0.36.0 Python API for solving
-  maze.
-  init parameters
-  ---------------
-  xml : str (required)
-    Mission setting (XML string) used in Project Malmo.
-  width : int (required)
-    Frame width for agent
-  height : int (required)
-    Frame height for agent.
-  millisec_per_tick : int (optional)
-    Millisec between each ticks. Set lower value to speed up.
-    Default is 50 (which is normal Minecraft game speed).
-  mazeseed : str/int (optional)
-    Seed value for maze. To create the same maze, set same value of seed.
-    Default is "random".
-  """
-  def __init__(self,
-    xml,
-    width,
-    height,
-    millisec_per_tick = 50,
-    mazeseed = "random"):
-    # Set up gym.Env
-    super(MalmoMazeEnv, self).__init__()
-    # Initialize self variables
-    self.xml = xml
-    self.height = height
-    self.width = width
-    self.shape = (self.height, self.width, 3)
-    self.millisec_per_tick = millisec_per_tick
-    self.mazeseed = mazeseed
-    # none:0, move:1, right:2, left:3
-    self.action_space = AgentActionSpace()
-    # frame
-    self.observation_space = gym.spaces.Box(low=0, high=255, shape=self.shape, dtype=np.float32)
-    # Create AgentHost
-    self.agent_host = MalmoPython.AgentHost()
-    # Create MissionRecordSpec
-    self.my_mission_record = MalmoPython.MissionRecordSpec()
-    self.my_mission_record.recordRewards()
-    self.my_mission_record.recordObservations()
-    # Create ClientPool
-    self.pool = MalmoPython.ClientPool()
-    client_info = MalmoPython.ClientInfo('127.0.0.1', CLIENT_PORT)
-    self.pool.add(client_info)
+    """
+    A class implementing OpenAI gym environment to
+    run Project Malmo 0.36.0 Python API for solving
+    maze.
+    init parameters
+    ---------------
+    xml : str (required)
+        Mission setting (XML string) used in Project Malmo.
+    width : int (required)
+        Frame width for agent
+    height : int (required)
+        Frame height for agent.
+    millisec_per_tick : int (optional)
+        Millisec between each ticks. Set lower value to speed up.
+        Default is 50 (which is normal Minecraft game speed).
+    mazeseed : str/int (optional)
+        Seed value for maze. To create the same maze, set same value of seed.
+        Default is "random".
+    enable_action_history : bool (optional)
+        Set if the action is recorded in "action_history" attribute.
+        Default is False.
+    """
+    def __init__(self,
+        xml,
+        width,
+        height,
+        millisec_per_tick = 50,
+        mazeseed = "random",
+        enable_action_history=False):
+        # Set up gym.Env
+        super(MalmoMazeEnv, self).__init__()
+        # Initialize self variables
+        self.xml = xml
+        self.height = height
+        self.width = width
+        self.shape = (self.height, self.width, 3)
+        self.millisec_per_tick = millisec_per_tick
+        self.mazeseed = mazeseed
+        self.enable_action_history = enable_action_history
+        # none:0, move:1, right:2, left:3
+        self.action_space = AgentActionSpace()
+        # frame
+        self.observation_space = gym.spaces.Box(low=0, high=255, shape=self.shape, dtype=np.float32)
+        # Create AgentHost
+        self.agent_host = MalmoPython.AgentHost()
+        # Create MissionRecordSpec
+        self.my_mission_record = MalmoPython.MissionRecordSpec()
+        self.my_mission_record.recordRewards()
+        self.my_mission_record.recordObservations()
+        # Create ClientPool
+        self.pool = MalmoPython.ClientPool()
+        client_info = MalmoPython.ClientInfo('127.0.0.1', CLIENT_PORT)
+        self.pool.add(client_info)
 
-  """
-  Public methods
-  """
+    """
+    Public methods
+    """
 
-  def reset(self):
-    # Create MissionSpec
-    xml = self.xml
-    xml = xml.format(
-      PLACEHOLDER_MSPERTICK=self.millisec_per_tick,
-      PLACEHOLDER_WIDTH=self.width,
-      PLACEHOLDER_HEIGHT=self.height,
-      PLACEHOLDER_MAZESEED=self.mazeseed)
-    my_mission = MalmoPython.MissionSpec(xml,True)
-    # Start mission
-    self.agent_host.startMission(my_mission,
-      self.pool,
-      self.my_mission_record,
-      0,
-      'test1')
-    # Wait till mission begins
-    world_state = self.agent_host.getWorldState()
-    while not world_state.has_mission_begun:
-      time.sleep(TIME_WAIT * self.millisec_per_tick / 20)
-      world_state = self.agent_host.getWorldState()
-    # Get reward, done, and frame
-    frame, _, _ = self._process_state(False)
-    if frame is None:
-      self.last_obs = np.zeros(self.shape, dtype=np.float32)
-    else:
-      self.last_obs = np.frombuffer(frame.pixels, dtype=np.uint8).reshape(self.shape)
-    return self.last_obs
+    def reset(self):
+        # Create MissionSpec
+        xml = self.xml
+        xml = xml.format(
+            PLACEHOLDER_MSPERTICK=self.millisec_per_tick,
+            PLACEHOLDER_WIDTH=self.width,
+            PLACEHOLDER_HEIGHT=self.height,
+            PLACEHOLDER_MAZESEED=self.mazeseed)
+        my_mission = MalmoPython.MissionSpec(xml,True)
+        # Start mission
+        self.agent_host.startMission(my_mission,
+            self.pool,
+            self.my_mission_record,
+            0,
+            'test1')
+        # Wait till mission begins
+        world_state = self.agent_host.getWorldState()
+        while not world_state.has_mission_begun:
+            time.sleep(TIME_WAIT * self.millisec_per_tick / 20)
+            world_state = self.agent_host.getWorldState()
+        # Get reward, done, and frame
+        frame, _, _ = self._process_state(False)
+        if frame is None:
+            self.last_obs = np.zeros(self.shape, dtype=np.float32)
+        else:
+            self.last_obs = np.frombuffer(frame.pixels, dtype=np.uint8).reshape(self.shape)
+        # Record action history
+        if self.enable_action_history:
+            self.action_history = []
+        return self.last_obs
 
-  def step(self, action):
-    # Take corresponding actions
-    """ none:0, move:1, right:2, left:3 """
-    if self.action_space[action] == "move":
-      self.agent_host.sendCommand("move 1")
-    elif self.action_space[action] == "right":
-      self.agent_host.sendCommand("turn 1")
-    elif self.action_space[action] == "left":
-      self.agent_host.sendCommand("turn -1")
+    def render(self, mode=None):
+        if self.last_obs is None:
+            self.last_obs = np.zeros(self.shape, dtype=np.float32)
+        return np.flipud(self.last_obs)
 
-    # Get reward, done, and frame
-    frame, reward, done = self._process_state()
-    if reward is None:
-      reward = 0
-    # Clean up
-    if done:
-      frame2, reward2 = self._comsume_state()
-      if frame2 is not None:
-        frame = frame2
-      reward = reward + reward2
-    # Return observations
-    if frame is None:
-      self.last_obs = np.zeros(self.shape, dtype=np.uint8)
-    else:
-      self.last_obs = np.frombuffer(frame.pixels, dtype=np.uint8).reshape(self.shape)
-    return self.last_obs, reward, done, {}
+    def step(self, action):
+        # Take corresponding actions
+        """ none:0, move:1, right:2, left:3 """
+        if self.action_space[action] == "move":
+            self.agent_host.sendCommand("move 1")
+        elif self.action_space[action] == "right":
+            self.agent_host.sendCommand("turn 1")
+        elif self.action_space[action] == "left":
+            self.agent_host.sendCommand("turn -1")
 
-  """
-  Internal methods
-  """
+        # Get reward, done, and frame
+        frame, reward, done = self._process_state()
+        if reward is None:
+            reward = 0
+        # Clean up
+        if done:
+            frame2, reward2 = self._comsume_state()
+            if frame2 is not None:
+                frame = frame2
+            reward = reward + reward2
+        # Return observations
+        if frame is None:
+            self.last_obs = np.zeros(self.shape, dtype=np.uint8)
+        else:
+            self.last_obs = np.frombuffer(frame.pixels, dtype=np.uint8).reshape(self.shape)
+        # Record action history
+        if self.enable_action_history:
+            self.action_history.append(action)
+        return self.last_obs, reward, done, {}
 
-  # Extract frames, rewards, done_flag
-  def _process_state(self, get_reward=True):
-    reward_flag = False
-    reward = 0
-    frame_flag = False
-    frame = None
-    done = False
-    loop = 0
-    while True:
-      # get world state
-      time.sleep(TIME_WAIT * self.millisec_per_tick / 20)
-      world_state = self.agent_host.getWorldState()
-      # reward (loop till command's rewards are all retrieved)
-      if (not reward_flag) and (world_state.number_of_rewards_since_last_state > 0):
-        reward_flag = True;
-        reward = reward + world_state.rewards[-1].getValue()
-      # frame
-      if world_state.number_of_video_frames_since_last_state > 0:
-        frame = world_state.video_frames[-1]
-        frame_flag = True
-      # done flag
-      done = not world_state.is_mission_running
-      # judge quit
-      if get_reward and reward_flag and frame_flag:
-        break;
-      elif (not get_reward) and frame_flag:
-        break;
-      # exit when MAX_LOOP exceeds
-      loop = loop + 1
-      if loop > MAX_LOOP:
-        reward = None
-        break;
-    return frame, reward, done
+    def close(self):
+        self.agent_host.sendCommand("quit")
+        _, _, _ = self._process_state()
+        _, _ = self._comsume_state()
 
-  def _comsume_state(self):
-    reward_flag = True
-    reward = 0
-    frame = None
-    loop = 0
-    while True:
-      # get next world state
-      time.sleep(TIME_WAIT * self.millisec_per_tick / 5)
-      world_state = self.agent_host.getWorldState()
-      # reward (loop till command's rewards are all retrieved)
-      if reward_flag and not (world_state.number_of_rewards_since_last_state > 0):
-        reward_flag = False;
-      if reward_flag:
-        reward = reward + world_state.rewards[-1].getValue()
-      # frame
-      if world_state.number_of_video_frames_since_last_state > 0:
-        frame = world_state.video_frames[-1]
-      if not reward_flag:
-        break;
-    return frame, reward
+    """
+    Internal methods
+    """
+
+    # Extract frames, rewards, done_flag
+    def _process_state(self, get_reward=True):
+        reward_flag = False
+        reward = 0
+        frame_flag = False
+        frame = None
+        done = False
+        loop = 0
+        while True:
+            # get world state
+            time.sleep(TIME_WAIT * self.millisec_per_tick / 20)
+            world_state = self.agent_host.getWorldState()
+            # reward (loop till command's rewards are all retrieved)
+            if (not reward_flag) and (world_state.number_of_rewards_since_last_state > 0):
+                reward_flag = True;
+                reward = reward + world_state.rewards[-1].getValue()
+            # frame
+            if world_state.number_of_video_frames_since_last_state > 0:
+                frame = world_state.video_frames[-1]
+                frame_flag = True
+            # done flag
+            done = not world_state.is_mission_running
+            # (Do not quit before comsuming)
+            # if done:
+            #     break;
+            # exit loop when extraction is completed
+            if get_reward and reward_flag and frame_flag:
+                break;
+            elif (not get_reward) and frame_flag:
+                break;
+            # exit when MAX_LOOP exceeds
+            loop = loop + 1
+            if loop > MAX_LOOP:
+                reward = None
+                break;
+        return frame, reward, done
+
+    def _comsume_state(self):
+        reward_flag = True
+        reward = 0
+        frame = None
+        loop = 0
+        while True:
+            # get next world state
+            time.sleep(TIME_WAIT * self.millisec_per_tick / 5)
+            world_state = self.agent_host.getWorldState()
+            # reward (loop till command's rewards are all retrieved)
+            if reward_flag and not (world_state.number_of_rewards_since_last_state > 0):
+                reward_flag = False;
+            if reward_flag:
+                reward = reward + world_state.rewards[-1].getValue()
+            # frame
+            if world_state.number_of_video_frames_since_last_state > 0:
+                frame = world_state.video_frames[-1]
+            if not reward_flag:
+                break;
+        return frame, reward
---
+++ basic_agent.py
@@ -0,0 +1,90 @@
+import gym
+from functools import partial
+import argparse
+from pathlib import Path
+import random
+import time
+import os
+
+from yaml_config_wrapper import Configuration
+from RLcraft import MalmoMazeEnv
+
+
+def create_env(config):
+    """ Create a custom OpenAI gym environment (custom MalmoMazeEnv). """
+    xml = Path(config["mission_file"]).read_text()
+    env = MalmoMazeEnv(
+        xml=xml,
+        width=config["width"],
+        height=config["height"],
+        millisec_per_tick=config["millisec_per_tick"])
+    return env
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    # Required Args
+    required_args = parser.add_argument_group('Required Arguments')
+    config_file_params = {
+        'type': argparse.FileType('r'),
+        'required': True,
+        'help': "The configuration yml file"
+    }
+    required_args.add_argument('-c', '--config-file', **config_file_params)
+    # Optional args
+    optional_args = parser.add_argument_group('Optional Arguments')
+    optional_args.add_argument("--num_gpus",
+                               type=int,
+                               required=False,
+                               default=0,
+                               help="number of gpus to use for trianing")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    """ Run a the game with a random agent. """
+    # Read command arguments
+    args = get_args()
+    # Load YML config file
+    c = Configuration(config_src=args.config_file)
+    # Load configs from config class
+    c_general = c.get_config('general')[0]
+    c_tuner = c.get_config('tuner')[0]
+    # Load the values from the config
+    run_config = c_tuner['config']
+    c_general = c_general['config']
+
+    run = True
+    while run:
+        # Generate a seed for maze 
+        print("Generating new seed ...")
+        maze_seed = random.randint(1, 9999)
+        print("Loading environment ...")
+        xml = Path(run_config["env_config"]["mission_file"]).read_text()
+        env = MalmoMazeEnv(
+            xml=xml,
+            width=800,
+            height=600,
+            millisec_per_tick=50,
+            mazeseed=maze_seed)
+        print("Resetting environment ...")
+        env.reset()
+        print("The world is loaded.")
+        print("Press Enter and F5 key in Minecraft to show third-person view.")
+        input("Press any key to start simulation")
+        done = False
+        while not done:
+            action = env.action_space.sample()
+            obs, reward, done, info = env.step(action)
+            env.render()
+        user_choice = input("Enter 'N' to exit, 'Y' to run new episode [Y/n]: ").lower()
+        if user_choice.lower() in ['n', 'no']:
+            run = False
+        env.close()
+    
+    print("Done.")
+
+
+if __name__ == '__main__':
+    main()
